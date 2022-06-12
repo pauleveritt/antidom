2c84a7f31828f068f3e1f78c50143b97de7a5fc3
pauleveritt <pauleveritt@me.com>
2022-06-12 14:28:21 -0400

    Remove fossil.

--- antidom/resource.py
+++ antidom/resource.py
@@ -1,6 +1,11 @@
 """Define and fetch resources from a resource tree."""
 from __future__ import annotations
-from typing import Protocol, Callable, Optional
+
+from copy import copy
+from typing import Protocol
+
+from antidote import world, inject, Provide
+from antidote.core import Provider, DependencyValue, Container
 
 
 class Resource(Protocol):
@@ -9,13 +14,32 @@ class Resource(Protocol):
     parent: Resource | None
 
 
-def current_resource() -> Callable[[Resource | None], Resource | None]:
-    cr: Resource | None = None
+@world.provider
+class ResourceProvider(Provider[str]):
+    _current_resource: Resource | None
 
-    def _current_resource(new_resource: Resource | None = None) -> Resource | None:
-        nonlocal cr
-        if new_resource is not None:
-            cr = new_resource
-        return cr
+    def __init__(self, first_resource: Resource | None = None) -> None:
+        super().__init__()
+        self._current_resource = first_resource
 
-    return _current_resource
+    def clone(self, keep_singletons_cache: bool) -> ResourceProvider:
+        this_copy = None if self._current_resource is None else copy(self._current_resource)
+        return ResourceProvider(first_resource=this_copy)
+
+    def exists(self, dependency: object) -> bool:
+        return dependency is Resource
+
+    def provide(self, dependency: str, container: Container) -> DependencyValue:
+        """Actually get the value."""
+        return DependencyValue(self._current_resource, scope=None)
+
+    def add_resource(self, resource: Resource) -> None:
+        self._current_resource = resource
+
+
+@inject
+def add_resource(new_resource: Resource,
+                 provider: Provide[ResourceProvider] | None = None,
+                 ) -> None:
+    assert provider is not None
+    provider.add_resource(new_resource)
--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,14 +1,29 @@
+from typing import cast
+
+from antidote import world
+
 from antidom.examples import Customer
-from antidom.resource import current_resource
+from antidom.resource import Resource, add_resource
 
 
-def test_current_resource() -> None:
-    cr = current_resource()
-    result = cr(None)
-    assert None is result
+# def test_current_resource() -> None:
+#     cr = current_resource()
+#     result = cr(None)
+#     assert None is result
+#
+#     # Now set a value
+#     this_resource = Customer(name='This Customer', parent=None)
+#     result = cr(this_resource)
+#     assert this_resource == result
 
-    # Now set a value
-    this_resource = Customer(name='This Customer', parent=None)
-    result = cr(this_resource)
-    assert this_resource == result
 
+def test_get_resource() -> None:
+    # We haven't added a resource to the provider yet
+    this_customer = Customer(name='First Customer', parent=None)
+    this_resource = world.get[Resource]  # type: ignore
+    assert this_resource(Resource) is None
+
+    # Now add the resource
+    add_resource(this_customer)
+    this_resource = world.get[Resource]  # type: ignore
+    assert this_resource(Resource) == this_customer
